d170ac66ee77aed18e2efd01bcafdb6d1c020fed
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-09-07 22:37:53 +0200

    simplified run.sh

--- run.sh
+++ run.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 # Original code: Nguyen Khac Trung Kien
 # Fork by: Felipe Avelar
+# Simplified for curl funny usage by LeSpatiocorne 
+
+#!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-
-usage() {
-    echo
-    echo "Usage: $0"
-    echo "  -h, --help  Display this help message"
-    echo
-    exit 1
-}
-
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-    usage
-fi
-
-echo
-
-read -p "Do you want to use mpv to play sound? You need mpv installed to do that. (y/n): " choice
-if [[ $choice =~ ^[Yy]$ ]]; then
-  # Check if mpv is installed
-  if ! command -v mpv &> /dev/null; then
-    echo
-    echo "mpv is not installed. Please install it to use this feature."
-    echo
-    exit 1
-  fi
-  mpv --no-video bad_apple.mp4 > /dev/null 2>&1 &
-fi
-
 dir="$SCRIPT_DIR/frames-ascii"
 
 clear
@@ -40,6 +16,5 @@ for filename in $(ls -v "$dir"); do
     if [ -f "$file" ]; then
         cat "$file"
     fi
-
     sleep 0.024
 done
