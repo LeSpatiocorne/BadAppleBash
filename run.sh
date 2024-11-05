32a58b16aa8f6b8ac04f5086d1d08e3e1641a184
LeSpatiocorne <97949654+LeSpatiocorne@users.noreply.github.com>
2024-11-05 13:02:43 +0100

    Update run.sh
    infinite loop

--- run.sh
+++ run.sh
@@ -9,12 +9,13 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 dir="$SCRIPT_DIR/frames-ascii"
 
 clear
-
-for filename in $(ls -v "$dir"); do
-    tput cup 0 0
-    file="$dir/$filename"
-    if [ -f "$file" ]; then
-        cat "$file"
-    fi
-    sleep 0.024
+while true; do
+    for filename in $(ls -v "$dir"); do
+        tput cup 0 0
+        file="$dir/$filename"
+        if [ -f "$file" ]; then
+            cat "$file"
+        fi
+        sleep 0.024
+    done
 done
